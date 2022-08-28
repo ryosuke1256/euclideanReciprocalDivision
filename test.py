3275841878a58dd8e9e06d18660f3750a4477441
Ryosuke <ryoll329555@docomo.ne.jp>
2022-08-28 21:14:40 +0900

    Update test.py

--- test.py
+++ test.py
@@ -3,10 +3,10 @@ import euclideanReciprocalDivision
 
 class TestEuclideanReciprocalDivision(unittest.TestCase):
     def test_euclidean_reciprocal_division(self):
-        self.assertTrue(euclideanReciprocalDivision.euclideanReciprocalDivision(51,21) == 3)
-        self.assertTrue(euclideanReciprocalDivision.euclideanReciprocalDivision(49,21) == 7)
-        self.assertTrue(euclideanReciprocalDivision.euclideanReciprocalDivision(21,49) == 7)
-        self.assertTrue(euclideanReciprocalDivision.euclideanReciprocalDivision(300,630) == 30)
-
+        self.assertEqual(euclideanReciprocalDivision.euclideanReciprocalDivision(51,21),3)
+        self.assertEqual(euclideanReciprocalDivision.euclideanReciprocalDivision(49,21),7)
+        self.assertEqual(euclideanReciprocalDivision.euclideanReciprocalDivision(21,49),7)
+        self.assertEqual(euclideanReciprocalDivision.euclideanReciprocalDivision(300,630),30)
+    
 if __name__ == '__main__':
     unittest.main()
