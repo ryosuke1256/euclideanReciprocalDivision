86a713308cdcc6ef90679e1dffd47ffb80e7f55e
Ryosuke <ryoll329555@docomo.ne.jp>
2022-08-31 11:21:52 +0900

    Update euclideanReciprocalDivision.py

--- euclideanReciprocalDivision.py
+++ euclideanReciprocalDivision.py
@@ -1,13 +1,7 @@
 def euclideanReciprocalDivision(x,y):
-    if x >= y:
-        largerValue = x
-        smallerValue = y
-    else:
-        largerValue = y
-        smallerValue = x
-    remainder = largerValue % smallerValue
+    remainder = x % y
     if remainder == 0:
-        return smallerValue
-    return euclideanReciprocalDivision(smallerValue, remainder)
+        return y 
+    return euclideanReciprocalDivision(y, remainder)
 x,y = map(int, input('正の整数を2つ入力してください: ').split())
 print('最大公約数は: ' + str(euclideanReciprocalDivision(x,y)))
